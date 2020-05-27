5f887fa99e7eb279e7d6edcb2e73db76101fba19
jakep <parlaj@oregonstate.edu>
2020-05-26 21:20:40 -0700

    Created Person super class with subclasses Human and Zombie

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,8 @@
 public class Human extends Person {
+    public Human() {
+    }
+    public Human(float position, float size) {
+        setPosition(position);
+        setSize(size);
+    }
 }
--- src/Zombie.java
+++ src/Zombie.java
@@ -1,2 +1,8 @@
 public class Zombie extends Person {
+    public Zombie() {
+    }
+    public Zombie(float position, float size) {
+        setPosition(position);
+        setSize(size);
+    }
 }
